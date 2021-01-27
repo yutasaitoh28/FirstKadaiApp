c245c8304668e7df8691070ce610f143fc68d6cc
齋藤雄太 <yutasaitoh28@arsaga.jp>
2021-01-27 10:39:34 +0900

    first commit

--- FirstKadaiApp/ViewController.swift
+++ FirstKadaiApp/ViewController.swift
@@ -14,8 +14,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let label = UILabel()
-        label.font = .systemFont(ofSize: 30)
+        
+        firstApp.font = firstApp.font.withSize(30)
+        
     }
 
 
